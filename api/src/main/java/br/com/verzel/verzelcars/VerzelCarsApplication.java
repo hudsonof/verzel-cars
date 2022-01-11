9a7b1c9f37fc86cf7af8e51c89412c8c89cade27
Hudson Oliveira <hudson.o.f@gmail.com>
2022-01-11 20:03:29 -0300

    commit inicial

---
+++ api/src/main/java/br/com/verzel/verzelcars/VerzelCarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.verzel.verzelcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerzelCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerzelCarsApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/verzel/verzelcars/VerzelCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.verzel.verzelcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerzelCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
